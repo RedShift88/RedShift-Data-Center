1d72d7abb11bc6b4ab31dd5255caabf06a7affaf
JDawsonTechnos <jdawson@technosfans.com>
2018-01-30 15:50:45 -0600

    Update to Service Tech Registration Flow
    Added email field to email the tech for user verification

--- src/main/java/com/redshift/redshiftdatacenter/createServiceTech.java
+++ src/main/java/com/redshift/redshiftdatacenter/createServiceTech.java
@@ -7,6 +7,10 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Text;
 
+import com.google.appengine.api.mail.MailServiceFactory;
+import com.google.appengine.api.mail.MailService;
+import com.google.appengine.api.mail.MailService.Message;
+
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -18,19 +22,26 @@ public class createServiceTech extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)throws IOException {
 		String name = req.getParameter("name");
-		String company = req.getParameter("company");
 		Text notes = new Text(req.getParameter("notes"));
 	
-		Key serviceTechKey = KeyFactory.createKey("ServiceTech", "default");
-		Entity ServiceTech = new Entity("ServiceTech", serviceTechKey);
-		
-		ServiceTech.setUnindexedProperty("Name", name);
-		ServiceTech.setUnindexedProperty("Company", company);
-		ServiceTech.setUnindexedProperty("Notes", notes);
-	
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		datastore.put(ServiceTech);
+		Key entityUserKey = KeyFactory.createKey("Entity User", "default");
+		Entity entityUser = new Entity("Entity User", entityUserKey);
+		
+		entityUser.setProperty("Name", name);
+		entityUser.setProperty("Parent Entity", entityKey);
+		entityUser.setProperty("Role", "Service Techician");
+	
+		datastore.put(entityUser);
+		
+		MailService mailService = MailServiceFactory.getMailService();
+		MailService.Message message = new MailService.Message();
+		message.setSender("techfanhubv2@appspot.gserviceaccount.com");
+		message.setSubject("Service Tech Registration");
+		message.setTo(req.getParameter("email"));
+		message.setHtmlBody("You are receiving this notification because you were setup as a service technician. <a href=\"https://redshift-data-center.appspot.com/Registration/Technician.jsp?key=" + KeyFactory.keyToString(entityUser.getKey())) + "\">Click here to register</a>. When prompted, please sign in using your Google account credentials and accept the default permissions. Thank you!");
+		mailService.send(message);
 
-		resp.sendRedirect("/serviceTechForm.jsp");
+		resp.sendRedirect("/entityAdmin/home.jsp");
 	}
 }
